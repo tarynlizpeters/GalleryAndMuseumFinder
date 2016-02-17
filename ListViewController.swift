5e5611ca34478e2e74f4a4a67e3c54a3694a424c
mouerj <joseph.mouer@icloud.com>
2016-02-17 11:48:58 -0600

    added code for locating galleries

--- ListViewController.swift
+++ ListViewController.swift
@@ -7,29 +7,75 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class GalleryListViewController: UIViewController {
 
+class GalleryListViewController: UIViewController, CLLocationManagerDelegate {
+var locationManager = CLLocationManager()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.first
+        
+        if location?.verticalAccuracy < 1000 && location?.horizontalAccuracy < 1000 {
+            locationManager.stopUpdatingLocation()
+        }
+        
     }
-    */
 
+    func reverseGeocoder(location: CLLocation) {
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { (placemarks:[CLPlacemark]?, error: NSError?) -> Void in
+            let placemark = placemarks?.first
+            let address = "\(placemark!.subThoroughfare!) \(placemark!.thoroughfare) \(placemark?.locality)"
+            print(address)
+            self.findGalleryNear(location)
+        }
+    }
+    
+    func findGalleryNear(location:CLLocation) {
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = "Gallery"
+        request.region = MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(1, 1))
+        let search = MKLocalSearch(request: request)
+        search.startWithCompletionHandler { (response:MKLocalSearchResponse?, error:NSError?) -> Void in
+            let mapItems = response?.mapItems
+            let mapItem = mapItems?.first
+            self.getDirectionsTo(mapItem!)
+        }
+    }
+
+    func getDirectionsTo(destinationItem: MKMapItem) {
+        let request = MKDirectionsRequest()
+        request.source = MKMapItem.mapItemForCurrentLocation()
+        let directions = MKDirections(request: request)
+        directions.calculateDirectionsWithCompletionHandler { (response:MKDirectionsResponse?, error:NSError?) -> Void in
+            let routes = response?.routes
+            let route = routes!.first
+            
+            var x = 1
+            let directionsString = NSMutableString()
+            for step in route!.steps {
+                directionsString.appendString("\(x): \(step.instructions)")
+                x++
+            }
+        }
+        
+    }
+    
+    
+    
 }
