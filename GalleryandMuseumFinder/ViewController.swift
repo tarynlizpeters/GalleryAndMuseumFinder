11669d5c5eda8769cbbd5954307fc2c79541d593
mouerj <joseph.mouer@icloud.com>
2016-02-22 18:22:27 -0600

    added map and table to same View Controller

--- GalleryandMuseumFinder/ViewController.swift
+++ GalleryandMuseumFinder/ViewController.swift
@@ -1,21 +1,31 @@
 //
-//  TableViewController.swift
-//  GalleryandMuseumFinder
+//  InitialViewController.swift
 //
-//  Created by Joseph Mouer on 2/21/16.
-//  Copyright © 2016 Mobile Makers. All rights reserved.
+//
+//  Created by Joseph Mouer on 2/22/16.
+//
 //
 
 import UIKit
+import GoogleMaps
+import Google
+import MapKit
 
-class TableViewController: UITableViewController {
+var tableView: UITableView!
+
+class ViewController: UIViewController, UIScrollViewDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: GMtableView!
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     var galleries = [NSDictionary]()
     var galleryArray  = [Gallery]()
-    
+
+ 
     override func viewDidLoad() {
-        
         super.viewDidLoad()
+        self.tableView.backgroundColor = UIColor.clearColor();
         self.tableView.rowHeight = 80
         let url = NSURL(string:"https://maps.googleapis.com/maps/api/place/textsearch/json?query=art+gallery+in+Chicago&key=AIzaSyDNopD2lCPhs0z-Uap3f8EPUt9R3gGjGjg")
         
@@ -33,31 +43,26 @@ class TableViewController: UITableViewController {
                 }
                 
                 dispatch_async(dispatch_get_main_queue()) { () -> Void in
-//                    self.tableView.reloadData()
+                    
                 }
                 
             }
             catch let error as NSError {
                 print("jsonError: \(error.localizedDescription)")
                 
-               
             }
-             self.tableView.reloadData()
+            self.tableView.reloadData()
         }
         
         task.resume()
     }
     
-    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
-        print(error)
-    }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return galleries.count
     }
     
-    
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("CellID", forIndexPath: indexPath)
         let gallery = galleries[indexPath.row] as NSDictionary
         cell.textLabel?.text = gallery.objectForKey("name") as? String
@@ -68,5 +73,17 @@ class TableViewController: UITableViewController {
         return cell
     }
     
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
+    }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        if (scrollView.contentOffset.y < self.mapView.frame.size.height * -1 ) {
+            scrollView .setContentOffset(CGPointMake(scrollView.contentOffset.x, self.mapView.frame.size.height * -1), animated: true)
+        }
+    }
     
 }
+
+
+
